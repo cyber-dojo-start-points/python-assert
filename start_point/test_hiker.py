a87ac3a889ca6f02cf002c6b4452767fac89954f
Jon Jagger <jon@jaggersoft.com>
2020-06-23 09:14:40 +0100

    Rename global-method to global-function

--- start_point/test_hiker.py
+++ start_point/test_hiker.py
@@ -1,17 +1,16 @@
 from hiker import global_answer, Hiker
 
-def test_global_method():
+def test_global_function():
     assert global_answer() == 42
 
 def test_instance_method():
     assert Hiker().instance_answer() == 42
-
 
 def green_traffic_light_pattern():
     return 'All tests passed'
 
 
 if __name__ == '__main__':
-    test_global_method()
+    test_global_function()
     test_instance_method()
     print(green_traffic_light_pattern())
